b7424a33a773871197ace99478d7d4b51b92be29
Lucas Brito <britolmbs@hotmail.com>
2024-06-22 14:56:14 -0300

    Colocando Animação no fundo e criando NavBar

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import NavBar from "../../components/NavBar/NavBar"
 import Hero from "./sections/Hero/Hero"
 
 const   Home= () =>  {
@@ -5,6 +6,7 @@ const   Home= () =>  {
 
   return (
     <>
+     <NavBar />
       <Hero />
     </>
   )
--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,8 +1,9 @@
-import { Button, Container, Grid, Typography, styled } from "@mui/material"
+import { Box, Button, Container, Grid, Typography, styled } from "@mui/material"
 import Avatar from "../../../../assets/images/Avatar.jpg"
 import DownloadIcon from '@mui/icons-material/Download';
 import MarkEmailUnreadIcon from '@mui/icons-material/MarkEmailUnread';
 import StyledButton from "../../../../components/StyledButton/StyledButton";
+import { AnimatedBackground } from "../../../../components/AnimateBackground/AnimateBackground";
 
 
 const   Hero = () =>  {
@@ -26,12 +27,19 @@ const   Hero = () =>  {
             <Container maxWidth="lg">
             <Grid container spacing={2}>
                 <Grid item xs={12}  md={5}>
-                <StyledImg src={Avatar} />
+                    <Box position="relative">
+                        <Box position="absolute" width={"150%"} top={-100} right={0}>
+<AnimatedBackground />
+                        </Box>
+                        <Box position="absolute" textAlign={"center"}><StyledImg src={Avatar} /></Box>
+                        
+                    </Box>
+                
                 </Grid>
                 <Grid item xs={12} md={7}>
-                    <Typography color={"primary.contrastText"}variant="h1"textAlign={"center"}>Lucas Brito</Typography>
+                    <Typography color={"primary.contrastText"}variant="h1"textAlign={"center"} pb={2}>Lucas Brito</Typography>
                     <Typography color={"primary.contrastText"}variant="h2"textAlign={"center"}>Desenvolvedor FullStack</Typography>
-                    <Grid container display={"flex"} justifyContent={"center"} spacing={3}>
+                    <Grid container display={"flex"} justifyContent={"center"} spacing={3} pt={3}>
                         <Grid item xs={12} md={4} display={"flex"} justifyContent={"center"}> 
                             
                         <StyledButton><DownloadIcon />
